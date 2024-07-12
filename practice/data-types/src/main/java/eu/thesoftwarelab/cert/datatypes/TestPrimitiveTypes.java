670c1990a37e9b709d1c558f7ecdda38538237cc
Emmanouil Georgopoulos <georgopoulosek@dias.com.gr>
2024-07-12 12:48:20 +0300

    Add primitive data types

--- practice/data-types/src/main/java/eu/thesoftwarelab/cert/datatypes/TestPrimitiveTypes.java
+++ practice/data-types/src/main/java/eu/thesoftwarelab/cert/datatypes/TestPrimitiveTypes.java
@@ -6,6 +6,17 @@ public class TestPrimitiveTypes {
     char c = 65;
     System.out.println("character Latin Capital letter A printed:" + c);
     char cMax = 65535; // max numeric value for char (0 to 2^16-1)
+
+    /**
+     * byte 8 bits, range -2^7 to 2^7-1
+     * char 16 bits, range 0 to 2^16-1
+     * short 16 bits, range -2^15 to 2^15-1
+     * int 32 bits, range -2^31 to 2^31-1
+     * long 64 bits, range -2^63 to 2^63-1
+     * float 32 bits
+     * double 64 bits
+     * boolean 1 bit
+     */
   }
 
 }
