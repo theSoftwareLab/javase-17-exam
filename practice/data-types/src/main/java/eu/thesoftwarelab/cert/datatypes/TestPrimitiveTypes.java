7cfdcbfbb7f080ff1f52ac59a357fb2338915244
Emmanouil Georgopoulos <georgopoulosek@dias.com.gr>
2024-07-12 12:18:06 +0300

    Add example for primitive type char

---
+++ practice/data-types/src/main/java/eu/thesoftwarelab/cert/datatypes/TestPrimitiveTypes.java
@@ -0,0 +1,11 @@
+package eu.thesoftwarelab.cert.datatypes;
+
+public class TestPrimitiveTypes {
+
+  public static void main(String[] args) {
+    char c = 65;
+    System.out.println("character Latin Capital letter A printed:" + c);
+    char cMax = 65535; // max numeric value for char (0 to 2^16-1)
+  }
+
+}
