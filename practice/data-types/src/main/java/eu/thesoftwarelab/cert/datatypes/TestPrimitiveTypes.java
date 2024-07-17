4b2ee0a09e54a2750ffa9ff16114cff771bd42eb
Emmanouil Georgopoulos <georgopoulosek@dias.com.gr>
2024-07-17 17:08:45 +0300

    Cast floating point value to an int

--- practice/data-types/src/main/java/eu/thesoftwarelab/cert/datatypes/TestPrimitiveTypes.java
+++ practice/data-types/src/main/java/eu/thesoftwarelab/cert/datatypes/TestPrimitiveTypes.java
@@ -7,6 +7,9 @@ public class TestPrimitiveTypes {
     System.out.println("character Latin Capital letter A printed:" + c);
     char cMax = 65535; // max numeric value for char (0 to 2^16-1)
 
+    int i = (int) 10.5;
+    System.out.println("casting the floating point value 10.5 to an int: " + i);
+
     /**
      * byte 8 bits, range -2^7 to 2^7-1
      * char 16 bits, range 0 to 2^16-1
